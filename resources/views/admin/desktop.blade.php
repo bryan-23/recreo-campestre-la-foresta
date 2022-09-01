bebdf077f41db3bb666e06d15019f948af1bf9b3
Bryan Roy <bryan.inga@unas.edu.pe>
2022-08-31 22:51:03 -0500

    base del proyecto

---
+++ resources/views/admin/desktop.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.config')
+@section('content')
+@include('admin.header')
+
+<div id="layoutSidenav">
+    @include('admin.sidebar')
+    <div id="layoutSidenav_content">
+        <hr>
+        <footer class="footer-admin mt-auto footer-light">
+            <div class="container-xl px-4">
+                <div class="row">
+                    <div class="col-md-6 small">Copyright © Your Website 2022</div>
+                    <div class="col-md-6 text-md-end small">
+                        <a href="#!">Privacy Policy</a>
+                        ·
+                        <a href="#!">Terms &amp; Conditions</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+
+@endsection
