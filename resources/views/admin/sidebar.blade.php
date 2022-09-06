73014ddb8c94a2f4d404b3431c1de82a4a898fa9
Bryan Roy <bryan.inga@unas.edu.pe>
2022-09-06 08:38:24 -0500

    modificacion del sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -10,17 +10,16 @@
                     </a>
                 </li>
                 <!-- Sidenav Menu Heading (admin)-->
-                <div class="sidenav-menu-heading">Administacion</div>
+                <div class="sidenav-menu-heading">Administracion</div>
                 <!-- Sidenav Link (Home)-->
                 <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse" data-bs-target="#collapseConfiguration" aria-expanded="false" aria-controls="collapseConfiguration">
                     <div class="nav-link-icon"><em class='bx bx-cog'></em></div>
-                    Configuración
+                    Mostrador
                     <div class="sidenav-collapse-arrow"><em class='bx bx-chevron-down' style="color:gray; font-size:16px;"></em></div>
                 </a>
                 <div class="collapse" id="collapseConfiguration" data-bs-parent="#accordionSidenav">
                     <nav class="sidenav-menu-nested nav accordion" id="accordionSidenavPages">
                         <li class="nav-item">
-                            <a class="nav-link" href="/admin/usuario">Mesa</a>
                         </li>
                     </nav>
                 </div>
