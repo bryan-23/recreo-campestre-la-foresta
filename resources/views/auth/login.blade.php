8ba072ff8c553f4a727944a3b752049b452aecee
Bryan Roy <bryan.inga@unas.edu.pe>
2022-09-06 09:02:45 -0500

    mejora del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,13 +6,15 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
-
+                <div class="text-center">
+                <img src="../imagenes/LOGIN.jpg" alt="" width="200" height="200">   
+                </div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email"  class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
