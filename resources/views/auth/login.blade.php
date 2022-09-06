ba68943759f4260db5aad2211d98a6a1de55ae7e
Bryan Roy <bryan.inga@unas.edu.pe>
2022-09-06 09:56:03 -0500

    imagenes de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,16 +5,18 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
                 <div class="text-center">
-                <img src="../imagenes/LOGIN.jpg" alt="" width="200" height="200">   
+                <div class="card-header">{{ __('BIENVENIDO') }}</div>
+                </div>
+                <div class="text-center">
+                <img src="../imagenes/LOGIN.jpg" alt="" width="300" height="300">   
                 </div>
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email"  class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email"  class="col-md-4 col-form-label text-md-end">{{ __('Usuario:') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -28,7 +30,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña:') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
