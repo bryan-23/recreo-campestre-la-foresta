70e61905ea4dda037144bd74a349a5cdf091c86b
Bryan Roy <bryan.inga@unas.edu.pe>
2022-09-13 09:16:11 -0500

    arreglo sistema de mozo

--- resources/views/admin/Platos/index.blade.php
+++ resources/views/admin/Platos/index.blade.php
@@ -10,50 +10,13 @@
                         <div class="row justify-content-center">
                             <div class="col-12 col-xl-auto">
                                 <h1 class="page-title">
-                                    MENUS DETERMINADOS
+                                    SISTEMA DE PEDIDOS
                                 </h1>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="page-body page-body-light pt-3 px-2">
-                    <div class="card card-header-actions">
-                        <div class="card-header">
-                            MENU A LA CARTA
-                            <a href="/admin/roles/addroles" class="btn btn-primary lift"><i class='bx bxs-bowl-hot'></i>{{ __('Nuevo') }}</a>
-                        </div>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end py-2 px-4">
-                            <form class="input-group">
-                                <input name="busqueda" class="form-control me-md-2" type="search"
-                                    placeholder="Ingrese nombre" aria-label="Search" autocomplete="off">
-
-                            </form>
-                        </div>
-                        <div class="card-body py-9">
-                            <table class="table table-sm table-bordered table-hover " id="datatablesSimple">
-                                <thead>
-                                    <tr>
-                                        <th>Menu</th>
-                                        <th>Usuarios</th>
-                                        <th>Costos</th>
-                                        <th>Pedidos</th>
-                                        
-                                    </tr>
-                                </thead>
-
-                                <tbody>
-                                    <tr>
-                                        <td>Nombre</td>
-                                        <td>Nombre y Apellido</td>
-                                        <td>Soles</td>
-                                        <td>Activo</td>
-                                        
-                                    </tr>
-                                </tbody>
-                            </table>
-
-                        </div>
-                    </div>
+             </div>
                 </div>
             </main>
         </div>
